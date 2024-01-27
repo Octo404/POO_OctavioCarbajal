9d3077bc98dbe4068fa1479520f0a34c606ddb64
Octavio <octavio.carb2005@gmail.com>
2024-01-26 19:09:10 -0600

    Reto 1 terminado y documentado

--- src/edu/octavio_carbajal/reto1/Main.java
+++ src/edu/octavio_carbajal/reto1/Main.java
@@ -3,6 +3,9 @@ package edu.octavio_carbajal.reto1;
 import edu.octavio_carbajal.reto1.ui.CLI;
 
 public class Main {
+    /**
+     *Esto permite dar inicio al programa
+     */
     public static void main(String[] args) {
         CLI.showMenu();
         CLI.lauchAPP();
--- src/edu/octavio_carbajal/reto1/ui/CLI.java
+++ src/edu/octavio_carbajal/reto1/ui/CLI.java
@@ -5,6 +5,11 @@ import edu.octavio_carbajal.reto1.process.CalculadoraMasa;
 import java.util.Scanner;
 
 public class CLI {
+    /**
+     * En esta clase se utilia para mostrar todos los datos al
+     * usurio y darle a escogern entre distintas opciones
+     * y tenga distintos resultados
+     */
     public static void showMenu(){
         System.out.println("******Bienvenido******");
         System.out.println("Escrinbe el numero de la opcion que deseas");
@@ -16,11 +21,18 @@ public class CLI {
 
     }
     public static void lauchAPP(){
+        /**
+         * Esta parte permite dar inicio a los que seria la interfas del usuario
+         */
         Scanner scanner = new Scanner(System.in);
         showMenu();
         int seleccion = 0;
         seleccion = scanner.nextInt();
         if (seleccion == 1) {
+            /**
+             * Aqui basicamente se le piden los datos al usuario
+             * y se almacenan para las operaciones
+             */
             System.out.println("Ingrese su altura en centimetros");
             double altura = scanner.nextDouble();
             System.out.println("Ingrese su peso en Kilogramos");
@@ -28,9 +40,39 @@ public class CLI {
             double imc = CalculadoraMasa.calcularMasaCorporal(peso, altura);
             System.out.println("tu imc es:"+imc);
         } else if (seleccion == 2) {
-            /*char sexo = scaner.next().charAt(0);*/
-
+            /**
+             * Aqui igualmente se almacenan los datos solicitados con
+             * la diferencia de que ahora se le pide al usuario el sexo
+             */
+            System.out.println("ingrese su peso en kilogramos");
+            double peso = scanner.nextDouble();
+            System.out.println("ingrese su altura el centimetros");
+            double altura = scanner.nextDouble();
+            System.out.println("¿cual el su sexo?");
+            System.out.println("m ó f");
+            char sexo = scanner.next().charAt(0);
+            double masaMagra = CalculadoraMasa.masaMagra(peso, altura, sexo);
+            System.out.println("Su masa corporal magra es"+ masaMagra);
+        } else if (seleccion == 3) {
+            /**
+             * Aqui se piden los dato al usuario agragando el dato de la edad
+             */
+            System.out.println("Ingrese su peso el kilogramos");
+            double peso = scanner.nextDouble();
+            System.out.println("Ingrese su altura en centimetros");
+            double altura = scanner.nextDouble();
+            System.out.println("Ingrese su edad");
+            int edad = (int) scanner.nextDouble();
+            System.out.println("Cual es su sexo");
+            System.out.println("M ó F");
+            char sexo = scanner.next().charAt(0);
+            double metabolismoBasal = CalculadoraMasa.metabolismoBasal(peso, altura, edad, sexo);
+            System.out.println("Su Metabolismo Basal es" + metabolismoBasal);
+        } else if (seleccion == 4) {
+            /**
+             * Esta opcion da fin al programa
+             */
+            System.out.println("Fin de Programa");
         }
-
     }
 }
